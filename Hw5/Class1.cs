0ad98b11138c9f2e70e56d2dbfca93cf0e2d3278
chusi <4us.in.ua@gmail.com>
2022-05-18 11:17:16 +0300

    Выполнил 2 Задания

---
+++ Hw5/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw5
+{
+   public static class Message    {
+        #region Задание 2
+        private static string[] separators = { ",", ".", "!", "?", "-", ";", ":", " " };
+        //Задание 2.А
+        public static void Printwords(string message,int countLeters)
+        {
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if(words[i].Length < countLeters) Console.WriteLine(words[i]);
+            }
+
+
+        }
+        //Задание 2.Б
+        public static void DeleteLastSymbol(string message, char symbol)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i][words[i].Length-1] != symbol)
+                {
+                    sb.Append(words[i] + " ");
+                }
+            
+            
+           }
+           
+        Console.WriteLine(sb.ToString());
+        
+        
+        }
+        //Задание 2.В и Г (решил совместить)
+        public static void MaxLength(string message)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            int maximumLenght=0, index=0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > maximumLenght) 
+                {
+                    maximumLenght = words[i].Length; 
+                    index=i;
+                }
+            }
+            for (int i = 0; i < words.Length; i++)
+            {
+
+                if (words[i].Length == maximumLenght)
+                {
+                    stringBuilder.Append(words[i] + " ");
+                }
+            }
+            Console.WriteLine($"Слово с максимальным количеством символов: {words[index]}");
+            Console.WriteLine("Слова с максимальным количеством символов:");
+            Console.WriteLine(stringBuilder.ToString());
+        }
+        #endregion
+    }
+}
+
--- Hw5/Program.cs
+++ Hw5/Program.cs
@@ -1,16 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hw5
 {
+    /// <summary>
+    /// Выполнил Цубера Кристиан
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
+            string test = "Привет меня зовут аня и елтонд";
+            string enterlog;
+            Console.WriteLine("Здравствуйте введите ваш логин");
+            enterlog = Console.ReadLine();
+            if (checkLogin(enterlog)) Console.WriteLine("Логин неверный");
+            if (!checkLogin(enterlog)) Console.WriteLine("Логин принят");
+            Console.ReadKey();
 
+            Message.DeleteLastSymbol(test, 'я');
+            Console.ReadLine();
+            Message.MaxLength(test);
+            Console.ReadLine();
         }
+
+
+
+
+
+
+
+        #region Задание 1
+        static bool checkLogin(string login)
+            {
+                char[] log = login.ToCharArray();
+
+                for (int i = 0; i < log.Length; i++)
+                {
+                    if (!char.IsLetterOrDigit(log[i]) || char.IsDigit(log[0]) || log.Length < 2 || log.Length > 10)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+        #endregion
+
+
+
+
+
+
     }
 }
